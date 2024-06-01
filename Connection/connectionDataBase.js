7499ccc3efc86a85697ffe65f80167ac9529b29c
Facundo Lozano <videoclipbanda2019@gmail.com>
2024-05-31 22:36:17 -0300

    Arreglo conexión base de datos

--- Connection/connectionDataBase.js
+++ Connection/connectionDataBase.js
@@ -1,14 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const connectionDatabase = new Sequelize("", "", "", {
-    host: "DESKTOP-0C34D10\SQLEXPRESS",
-    dialect: "mssql",
+const connectionDatabase = new Sequelize("tp2final", "root", "", {
+    host: "localhost",
+    dialect: "mysql",
+    port:"3306"
     
   });
 
   try {
     await connectionDatabase.authenticate();
-    console.log('Conección realizada con éxito a la base de datos.');
+    console.log('Conexión realizada con éxito a la base de datos.');
   } catch (error) {
     console.error('Error al intentar conectarse con la base de datos:', error);
   }
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import routes from "./Routes/routes.js";
+import connectionDatabase from "./connection/connectionDataBase.js";
 const app = express();
 
 app.use(express.json());
 
 app.use("/app", routes);
+
+await connectionDatabase.sync({force:false})
+
 
 app.listen(8080, () => {
     console.log(`🚀 Servidor abierto con éxito`);
