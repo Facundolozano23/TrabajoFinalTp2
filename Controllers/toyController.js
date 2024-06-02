ee6baff5104828df0e1bb2e5df9eab81a05964d9
Facundo Lozano <videoclipbanda2019@gmail.com>
2024-06-02 00:06:18 -0300

    Creación controller de toy

---
+++ Controllers/toyController.js
@@ -0,0 +1,34 @@
+import User from "../Models/Toy.js";
+
+class ToyController {
+  // createUser(){}
+  createToy = async (req, res) => {
+    try {
+      const {toyName, brand, price,minAge,size } = req.body;
+      const data = await Toy.create({ toyName, brand, price,minAge,size});
+      res.status(201).send({
+        success: true,
+        message: `Juguete ${data.toyNameName} creado con exito`,
+      });
+    } catch (error) {
+      res.status(400).send({ succces: false, message: error.message });
+    }
+  };
+
+  readAllToys = async (req, res) => {
+    try {
+      const data = await Toy.findAll({
+        attributes: ["toyName", "brand", "price","minAge","size"],
+      });
+      res.status(201).send({
+        success: true,
+        message: data,
+      });
+    } catch (error) {
+      res.status(400).send({ succces: false, message: error.message });
+    }
+  };
+
+ 
+}
+  export default ToyController;
