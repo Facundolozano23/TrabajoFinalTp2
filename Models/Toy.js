cb26ece4a5049ad2369090a4bc5876367ddcd046
Facundo Lozano <videoclipbanda2019@gmail.com>
2024-06-03 03:38:22 -0300

    Modificación en el modelo de Toy

--- Models/Toy.js
+++ Models/Toy.js
@@ -1,12 +1,15 @@
 import { DataTypes,Model } from "sequelize";
 import connectionDatabase from "../connection/connectionDataBase.js";
+
 class Toy extends Model {}
 
     Toy.init({
 
         id:{
             type: DataTypes.INTEGER,
+            primaryKey: true,
             autoIncrement: true,
+            allowNull:true
         },
         toyName:{
             type: DataTypes.STRING,
