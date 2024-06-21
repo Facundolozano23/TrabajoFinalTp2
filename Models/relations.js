c1bdcb628dfd1585533444ca0a2a29835eab2f26
Facundo Lozano <videoclipbanda2019@gmail.com>
2024-06-21 18:40:25 -0300

    Actualización de relaciones en la base de datos

--- Models/relations.js
+++ Models/relations.js
@@ -1,7 +1,38 @@
 import User from "./User.js";
 import Role from "./Role.js";
 import Toy from "./Toy.js";
-import Category from "./Category.js"
+import Category from "./Category.js";
+import Carrito from "./Carrito.js";
+import CarritoItem from "./CarritoItem.js";
+import Pedido from "./Pedido.js";
+
+
+Toy.hasMany(CarritoItem,{
+  foreignKey:"toyId",
+})
+CarritoItem.belongsTo(Toy,{
+  foreignKey:"toyId",
+})
+Carrito.hasOne(Pedido, {
+  foreignKey: "carritoId",
+});
+
+Pedido.belongsTo(Carrito, {
+  foreignKey: "carritoId",
+});
+User.hasMany(Carrito,{
+  foreignKey:"userId",
+})
+
+Carrito.belongsTo(User,{
+  foreignKey:"userId",
+})
+Carrito.hasMany(CarritoItem,{
+foreignKey:"carritoId",
+});
+CarritoItem.belongsTo(Carrito,{
+  foreignKey:"carritoId",
+})
 
 Category.hasMany(Toy,{
   foreignKey:"categoryId",
@@ -18,4 +49,4 @@ User.belongsTo(Role, {
   foreignKey: "roleId",
 });
 
-export { User, Role, Toy, Category};
+export { User, Role, Toy, Category, Carrito, CarritoItem, Pedido};
