d52e4cccf86b0d88b3d9886a4682fee74eaccf56
Facundo Lozano <videoclipbanda2019@gmail.com>
2024-06-21 04:47:39 -0300

    Actualización Proyecto

--- Routes/routes.js
+++ Routes/routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import userRoutes from "./userRoutes.js";
 import toyRoutes from "./toyRoutes.js";
 import categoryRoutes from "./categoryRoutes.js"
+import roleRoutes from "./roleRoutes.js";
 
 const routes = Router();
 
 routes.use("/user", userRoutes);
 routes.use("/toy", toyRoutes);
-routes.use("/category", categoryRoutes)
-
+routes.use("/category", categoryRoutes);
+routes.use("/role", roleRoutes);
 export default routes;
