6dd61ebcb8787dcda50f1f32abce2ad266f13fb4
Facundo Lozano <videoclipbanda2019@gmail.com>
2024-06-24 18:25:00 -0300

    Creación screen de editToy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import GetToys from './screens/GetToys';
 import ToyDetails from './screens/ToyDetails';
 import CreateToy from './screens/CreateToy';
 import EditToy from './screens/EditToy'
+import NavBar from'./components/NavBar'
+import Footer from './components/Footer';
 
 
 
@@ -13,12 +15,14 @@ import EditToy from './screens/EditToy'
 const App=()=> {
   return (
  <BrowserRouter>
+ <NavBar/>
    <Routes>
         <Route exact path='/toy' element={<GetToys />} />
         <Route exact path='/toy/:id' element={<ToyDetails />} />
         <Route exact path='/toy/create' element={<CreateToy />} />
         <Route exact path='/toy/edit/:id' element={<EditToy />} />
   </Routes>
+  <Footer />
     </BrowserRouter>
   );
 }
